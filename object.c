9786f4dabe957fcda4c3c8c790f19e79c952c1cd
Ananya Raghavendra <mitochondria4566@gmail.com>
2026-04-21 10:20:23 +0530

    Phase 1: implemented object_read and hash verification

--- object.c
+++ object.c
@@ -8,6 +8,7 @@
 //
 // PROVIDED functions: compute_hash, object_path, object_exists, hash_to_hex, hex_to_hash
 // TODO functions:     object_write, object_read
+// Phase 1: object_read with integrity verification
 
 #include "pes.h"
 #include <stdio.h>
